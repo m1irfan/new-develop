80ff97283220e88e40de1ae2dfed7251ad454382
m1irfan <irfan.sssit@gmail.com>
2024-07-26 14:15:19 +0530

    commit this code

--- myapp/src/myapp/Demo.java
+++ myapp/src/myapp/Demo.java
@@ -2,6 +2,8 @@ package myapp;
 
 public class Demo {
  // C:\Users\243597\git\repository3
+	
+	// https://www.youtube.com/watch?v=B2N1AKIQfnY&ab_channel=TechWebDocs
 	public static void main(String[] args) {
 		
 		System.err.println("working...........");
